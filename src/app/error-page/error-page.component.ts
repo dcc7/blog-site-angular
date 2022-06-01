adf883140a4c51f976bd159c77a691ee42ff2bae
dcc7 <58867358+dcc7@users.noreply.github.com>
2022-06-01 10:13:43 +1000

    error page changes

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    setTimeout(() =>{
+      this.router.navigate(['/']);
+    }, 2500)
   }
 
 }
